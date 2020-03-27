12128d15ee6cbe1ef2252452fa82d048cad79ed1
Scott Key <scottdkey@gmail.com>
2020-03-27 12:54:21 -0600

    removed divider semantic from Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Divider, Header } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 
 const Home = () => (
   <Container>
